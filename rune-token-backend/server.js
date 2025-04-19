a9bac928c7c417b149c3e7ce67e0247699020ea5
Monu Yadav <monuyadav60010@gmail.com>
2025-04-19 22:55:41 +0530

    linting code

--- rune-token-backend/server.js
+++ rune-token-backend/server.js
@@ -1,25 +1,6 @@
 import express from 'express'
-import {
-  Etching,
-  EtchInscription,
-  Rune,
-  Runestone,
-  Terms,
-  Range,
-  none,
-  some,
-  RuneId,
-  Edict
-} from 'runelib'
-import { script, payments, Psbt, networks } from 'bitcoinjs-lib'
-import * as bitcoin from 'bitcoinjs-lib'
-import * as tinysecp from 'tiny-secp256k1'
 import cors from 'cors'
-import axios from 'axios'
-import { ECPairFactory } from 'ecpair'
-import * as ecc from 'tiny-secp256k1'
 import runeManager from './routes/rune.js'
-bitcoin.initEccLib(tinysecp)
 
 export const app = express()
 app.use(express.json())
